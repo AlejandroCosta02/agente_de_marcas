cc59362d173a0e7fe13e221d32e637efd85913e4
AlejandroCosta02 <costa.claudio.alejandro@gmail.com>
2025-06-16 20:34:20 -0300

    fix: set secure session cookies for production in NextAuth config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -91,6 +91,17 @@ export const authOptions: NextAuthOptions = {
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: process.env.NODE_ENV === 'production',
+      },
+    },
+  },
 };
 
 // export default withAuth(
